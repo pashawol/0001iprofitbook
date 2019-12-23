416db5df7518134e96f98a60b98119e53cd17712
Pavel <wol1414@gmail.com>
2019-12-24 01:23:04 +0300

    first commit

---
+++ sourse/js/common.js
@@ -0,0 +1,125 @@
+const JSCCommon = {
+	// часть вызов скриптов здесь, для использования при AJAX
+	btnToggleMenuMobile: [].slice.call(document.querySelectorAll(".toggle-menu-mobile--js")),
+	menuMobile: document.querySelector(".menu-mobile--js"),
+	menuMobileLink: [].slice.call(document.querySelectorAll(".menu-mobile--js ul li a")),
+	body: document.querySelector("body"),
+
+	modalCall() {
+		$(".link-modal").fancybox({
+			arrows: false,
+			infobar: false,
+			touch: false,
+			type: 'inline',
+			i18n: {
+				en: {
+					CLOSE: "Закрыть",
+					NEXT: "Вперед",
+					PREV: "Назад",
+					// PLAY_START: "Start slideshow",
+					// PLAY_STOP: "Pause slideshow",
+					// FULL_SCREEN: "Full screen",
+					// THUMBS: "Thumbnails",
+					// DOWNLOAD: "Download",
+					// SHARE: "Share",
+					// ZOOM: "Zoom"
+				},
+			},
+		});
+		$(".modal-close-js").click(function () {
+			$.fancybox.close();
+		})
+	},
+	// /magnificPopupCall
+
+	// табы  . 
+	tabscostume(tab) {
+		$('.' + tab + '__caption').on('click', '.' + tab + '__btn:not(.active)', function (e) {
+			$(this)
+				.addClass('active').siblings().removeClass('active')
+				.closest('.' + tab).find('.' + tab + '__content').hide().removeClass('active')
+				.eq($(this).index()).show().addClass('active');
+
+		});
+	},
+	// /табы  
+	inputMask() {
+		// mask for input
+		$('input[type="tel"]').attr("pattern", "[+][0-9]{1}[(][0-9]{3}[)][0-9]{3}-[0-9]{2}-[0-9]{2}").inputmask("+9(999)999-99-99");
+	}
+	// /inputMask
+
+};
+
+function eventHandler() {
+	// полифил для object-fit
+	objectFitImages();
+	// Picture element HTML5 shiv
+	document.createElement("picture");
+	// для свг
+	svg4everybody({});
+
+	JSCCommon.modalCall();
+
+	JSCCommon.tabscostume('tabs');
+
+	JSCCommon.inputMask();
+
+	// JSCCommon.CustomInputFile();
+	// добавляет подложку для pixel perfect
+	$(".main-wrapper").after('<div class="screen" style="background-image: url(screen/main.jpg);"></div>')
+	// /добавляет подложку для pixel perfect
+
+
+	// /закрыть/открыть мобильное меню
+
+	function heightses() {
+
+		const w = $(window).width();
+
+		// скрывает моб меню
+
+		const topH = $("header ").innerHeight();
+
+		$(window).scroll(function () {
+			if ($(window).scrollTop() > topH) {
+				$('.top-nav  ').addClass('fixed');
+			} else {
+				$('.top-nav  ').removeClass('fixed');
+			}
+		});
+
+	}
+
+	$(window).resize(function () {
+		heightses();
+
+	});
+
+	heightses();
+
+	// листалка по стр
+	$("  .scroll-link").click(function () {
+		const elementClick = $(this).attr("href");
+		const destination = $(elementClick).offset().top;
+
+		$('html, body').animate({ scrollTop: destination }, 1100);
+
+		return false;
+	});
+
+	$('.custom-select-js').selectize({
+		sortField: 'text'
+	});
+
+	// $(window).on("load", function () {
+	// 	$(".aside-menu").mCustomScrollbar({
+	// 		scrollbarPosition: "inside"
+	// 	});
+	// });
+};
+if (document.readyState !== 'loading') {
+	eventHandler();
+} else {
+	document.addEventListener('DOMContentLoaded', eventHandler);
+}
